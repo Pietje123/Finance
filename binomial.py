33ec058e6208f400af8914c5ae89a3eb8ee82b3f
ardanwan <jozsef68@gmail.com>
2020-02-11 15:25:34 +0100

    Volledig werkende binomial tree (stock price, option price en hedge parameter)

--- binomial.py
+++ binomial.py
@@ -1,17 +1,15 @@
 import numpy as np
 
 # Input variables
-K = 52
+K = 50
 S0 = 50
-r = 0.06
-sigma = 0.20
-dt = 0.25
+r = 0.10
+sigma = 0.40
+dt = 0.0833
 
 # Model Parameters
-u = 1.2
-# np.exp(sigma * np.sqrt(dt))
-d = 0.8
-# np.exp(-sigma * np.sqrt(dt))
+u = np.exp(sigma * np.sqrt(dt))
+d = np.exp(-sigma * np.sqrt(dt))
 
 class Tree:
 	def __init__(self, depth):
@@ -19,12 +17,14 @@ class Tree:
 		self.layers = []
 		self.create()
 		self.assign_children()
+		self.price_stocks(S0)
+		self.price_options(K, r)
 
 	def create(self):
 		for row in range(1, self.depth + 1):
 			layer = []
 			for i in range(row):
-				layer.append(Node(row))
+				layer.append(Node(row, i))
 			self.layers.append(layer)
 
 	def assign_children(self):
@@ -32,25 +32,60 @@ class Tree:
 			for j in range(len(self.layers[i])):
 				self.layers[i][j].children = [self.layers[i+1][j], self.layers[i+1][j+1]]
 
+	def price_stocks(self, S0):
+		self.layers[0][0].stock_price = S0
+
+		for layer in self.layers[:-1]:
+			for node in layer:
+				node.stock_pricer()
+
+	def price_options(self, K, r):
+		for layer in self.layers[::-1]:
+			for node in layer:
+				node.option_pricer(K, r)
+
 class Node:
-	def __init__(self, layer):
+	def __init__(self, layer, index):
 		self.stock_price = 0
 		self.option_price = 0
 		self.layer = layer
+		self.index = index
 		self.children = []
 
-	def option_pricer(self, r):
-		up = self.children[0].option_price
-		down = self.children[1].option_price
-		p = (np.exp(r * dt) - d) / (u - d)
-		print(up, down, p)
+	def __str__(self):
+		return f"Layer: {self.layer}, #{self.index+1}, S: {self.stock_price}, f: {self.option_price}"
 
+	def stock_pricer(self):
+		if len(self.children) == 2:
+			if self.children[0].stock_price == 0:
+				self.children[0].stock_price = u * self.stock_price
+			if self.children[1].stock_price == 0:
+				self.children[1].stock_price = d * self.stock_price
 
-		self.option_price = (p * up + (1 - p) * down) * np.exp(-r * dt)
+	def option_pricer(self, K, r):
+		if len(self.children) == 2:
+			up = self.children[0].option_price
+			down = self.children[1].option_price
+			p = (np.exp(r * dt) - d) / (u - d)
+			self.option_price = (p * up + (1 - p) * down) * np.exp(-r * dt)
+		else:
+			self.option_price = max(K - self.stock_price, 0)
 
-tree = Tree(4)
-tree.create()
-tree.assign_children()
+	def hedge(self):
+		if len(self.children) == 0:
+			print("Hedging not possible for option at expiry date.")
+		else:
+			df = self.children[0].option_price - self.children[1].option_price
+			ds = self.children[0].stock_price - self.children[1].stock_price
+			if ds == 0:
+				print("Stock prices have not yet been set.")
+			elif df == 0:
+				print("Hedging not possible as option has no value.")
+			else:
+				return (df / ds)
 
-print(tree.layers)
-print(tree.layers[0][0].children)
+tree = Tree(6)
+for layer in tree.layers:
+	for node in layer:
+		print(node)
+		print(f"Hedge parameter: {node.hedge()}")
